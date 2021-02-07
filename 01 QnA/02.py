ead69994b5de8a464f72481b8258c41339e5875c
Igor Vlasov <vlasov.work@gmail.com>
2021-02-07 12:48:09 +0000

    решение 2х задач

---
+++ 01 QnA/02.py
@@ -0,0 +1,12 @@
+a = int( input() )
+a1 = a//1000
+
+a3 = a//10 % 10
+a4 = a%10
+
+if a3 != 0 and a4 !=0: # a3 * a4 !=0
+  a = a - ( a3**2 * a4**2 )
+  print(a)
+else:
+  a = a - a1 ** 2
+  print(a)
