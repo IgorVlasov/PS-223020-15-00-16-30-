8c5dbb6faf355366836b5318bde4b4effe5dab45
Artem Biryukov <artem.b_2005@mail.ru>
2021-02-07 12:44:23 +0000

    ооо моя оборона

--- 01 QnA/2.py
+++ 01 QnA/2.py
@@ -1,25 +1,45 @@
+# num = int(input())
+# j = 0
+# ent = False
+# n = 0
+# if num > 5000 and num < 10000:
+# 	num = str(num)
+# 	for i in num:
+# 		j += 1
+# 		if j == 1:
+# 			n = i
+# 		if j > 2:
+# 			if j == 3 and i != '0':
+# 				f_1 = int(i)
+# 			if j == 4 and i != '0':
+# 				f_2 = int(i)
+# 				ent = True
+
+# num = int(num)
+
+# if ent == True:
+# 	print (f_1 , f_2)
+# 	c = num - ((f_1**2) * (f_2**2))
+# elif ent == False:
+# 	c = num - n**2
+# print(c)
+
+
+
+
+
 num = int(input())
-j = 0
-ent = False
-n = 0
-if num > 5000 and num < 10000:
-	num = str(num)
-	for i in num:
-		j += 1
-		if j == 1:
-			n = i
-		if j > 2:
-			if j == 3 and i != "0":
-				f_1 = int(i)
-			if j == 4 and i != 0:
-				f_2 = int(i)
-				ent = True
 
-num = int(num)
+n1 = num//1000
+n2 = num//10%10
+n3 = num%10
+print(n1,n2,n3)
 
-if ent == True:
-	print (f_1 , f_2)
-	c = num - ((f_1**2) * (f_2**2))
-elif ent == False:
-	c = num - n**2
-print(c)
+if n2*n3!=0 :
+	num-=(n2*2)*(n3**2)
+else:
+	num-=n1**2
+print(num)
+
+
+
--- 01 QnA/Ghoul.py
+++ 01 QnA/Ghoul.py
@@ -5,7 +5,7 @@ while num > 12 :
 	num_new = num - 7
 	print (num,'- 7 =',num_new)
 	num=num_new
-	time.sleep(0.05)
+	time.sleep(0.07)
 print("Я гуль")
 
 
