e80db221c46f5fd71dc60974ce31917c4da70775
Artem Biryukov <artem.b_2005@mail.ru>
2021-02-07 12:04:10 +0000

    привет неадекватному заказчику!!!

--- 5.py
+++ 5.py
@@ -1,2 +1,38 @@
-num = str(input())
+num = input()
+j = 1
+
+
+def check(x,y,z,w):
+	if x!=y and x!=z and x!=w and y!=z and y!=w and z!=w:
+		slove(x,y,z,w)
+	else:
+		print("задача успешно провалена")
+def maxInt(x,y,z,w):
+	if x>y and x>z and x>w:
+		maxInt(x,y,z,w)
+
+
+def minInt(i1,i2,i3,i4):
+	if i2<i3 and i2<i4:
+		sum(i2,i1,i3,i4)
+	elif 
+
+sum(n1,n2,n3,n4):
+	n1=str(n1)
+	n2=str(n2)
+	n3=str(n3)
+	n4=str(n4)
+	print(n1+n2=n3+n4)
+
+
+for i in num:
+	if j == 1:
+		n1=int(i)
+	elif j == 2:
+		n2=int(i)
+	elif j == 3:
+		n3=int(i)
+	elif j == 4:
+		n4=int(i)
+	j+=1
 
--- Ghoul.py
+++ Ghoul.py
@@ -1,6 +1,12 @@
+import time
+
 num = 1000
 while num > 12 :
 	num_new = num - 7
 	print (num,'- 7 =',num_new)
 	num=num_new
+	time.sleep(0.05)
 print("Я гуль")
+
+
+
