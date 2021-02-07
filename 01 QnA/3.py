bf5413bd9f1ebb2c95b959671f87adc45e7a9538
Artem Biryukov <artem.b_2005@mail.ru>
2021-02-07 13:13:56 +0000

    3

--- 01 QnA/3.py
+++ 01 QnA/3.py
@@ -1,20 +1,19 @@
 a, b, c = map(int, input().split())
 
 def check(x,y,z):
-	n=y**2+z**2
-	if x == n**0.5:
-		print("1")
-	else :
-		print("0")
-	return 0
+	if x == 90:
+		if y==z:
+			print("1")
+	
 
-if a == b or b == c or a == c:
-	if a > b or a > c :
-		check(a,b,c)
-	if b > a or b > c : 
-		check(b,a,c)
-	else:
-		check(c,a,b)
+if a + b + c == 180:
+	if a == b or b == c or a == c:
+		if a > b and a > c :
+			check(a,b,c)
+		if b > a and b > c : 
+			check(b,a,c)
+		else:
+			check(c,a,b)
 
 else :
 	print ("0")
