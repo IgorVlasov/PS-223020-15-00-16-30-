135ef407deb20a804a19778260460b2e927aeddb
Alex Skar <savinalex2605@gmail.com>
2021-02-14 13:33:16 +0000

    q

--- todo.py
+++ todo.py
@@ -7,7 +7,7 @@ exit - закрыть прогармму
 """
 
 todo = {}
-userUnswer = 0
+userUnswer = input().lower()
 
 print("Введите команду")
 print("Для вывода списка команд введите help")
@@ -21,8 +21,21 @@ while True:
   elif userUnswer == "help":
     print(HELP)
   elif userUnswer == "add":
-    print("Введите дату события")
+    print("Введите дату события в формате дд.мм.гггг")
     usrKey = input()
+
+  for i in usrKey.split(".")
+  for i in check:
+    if i.isdigit():
+      check = False
+    else:
+      check = True
+    
+
+  if check:
+    print("не верный формат даты")
+    continue
+
     print("Что нужно делать?")
     usrValue = input()
 
@@ -32,7 +45,7 @@ while True:
     print("Событие удалено")
   elif userUnswer == "show":
     print("\nУ вас запланировано:")
-    for i in todo.keys():
+    for i in sorted(todo.keys()):
       print("\t" + todo[i])
   else:
     print("Не корректная команда")
