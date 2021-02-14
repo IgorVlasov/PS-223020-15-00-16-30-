d4c4bce83f2e33b314871719ac0924de98c44a4c
Alex Skar <savinalex2605@gmail.com>
2021-02-14 12:50:00 +0000

    q

--- todo.py
+++ todo.py
@@ -21,11 +21,19 @@ while True:
   elif userUnswer == "help":
     print(HELP)
   elif userUnswer == "add":
+    print("Введите дату события")
+    usrKey = input()
+    print("Что нужно делать?")
+    usrValue = input()
+
+    todo[ usrKey ] = usrValue
     print("Событие добавлено")
   elif userUnswer == "remove":
     print("Событие удалено")
   elif userUnswer == "show":
-    print("Ку!")
+    print("\nУ вас запланировано:")
+    for i in todo.keys():
+      print("\t" + todo[i])
   else:
     print("Не корректная команда")
     print("Для вывода списка команд введите help ")
