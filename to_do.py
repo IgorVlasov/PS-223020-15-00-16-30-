be87319760a05ac7fdc82994ce4e103454c59ff1
plague doctor <plangedoctor44@gmail.com>
2021-02-14 13:33:16 +0000

    12

--- to_do.py
+++ to_do.py
@@ -13,19 +13,41 @@ print("Введите команду")
 print("Для вывода списка команд введите help")
 
 while True:
- userUnswer = input()
+ userUnswer = input().lower()
 
-if userUnswer =="exit":
+if userUnswer == "exit":
   print("программа закрыта")
   break
-elif userUnswer =="help":
+elif userUnswer == "help":
   print(HELP) 
-elif userUnswer =="add":
+elif userUnswer == "add":
+  print("Введите дату события в формате дд,мм,гггг")
+  usrKey = input()
+
+check = usrKey.split(".")
+for i in check:
+  print(i.sdigit())
+  if i.sdigit():
+    check = False
+  else:
+    check = True
+    
+if check:
+  print("Не верный формат даты")
+  continue
+
+ print("Что нужно сделать?")
+  usrValue = input()
+
+  todo[ usrKey ] = usrValue
   print("событие добавлено") 
-elif userUnswer =="remove":
+elif userUnswer == "remove":
   print("Событие удалено") 
-elif userUnswer =="show":
-  print("Ку!")
+elif userUnswer == "show":
+  print("\n Увас запланировано:")
+  
+  for i in sorted( todo.keys() ):
+    print(i + "\t" + todo.keys[i])
 else:
   print("Не коректная команда") 
   print("Для выводы списка команд введите help") 
