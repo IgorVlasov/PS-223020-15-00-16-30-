27404df9d265bf464cc073debeb4a5b0a2364bec
lehalehaleha112 <ashyrov20@mail.ru>
2021-02-14 13:33:15 +0000

    todo list

--- ToDo.py
+++ ToDo.py
@@ -13,7 +13,7 @@ print("Введите команду")
 print("для вывода сптска команд введите help")
 
 while True:
-  userUnswer = input()
+  userUnswer = input().lower()
 
   if userUnswer == "exit":
     print ("Программа закрыта")
@@ -22,14 +22,32 @@ while True:
     print (HELP)
 
   elif userUnswer == "add":
+    print("Введите дату события в формате дд.мм.гггг")
+    
+    userKey = input()
+
+    check = userKey.split(".")
+    for i in check:
+      if i.isdigit():
+        continue
+      else:
+        check = True
+        break
+    if check:
+      continue
+
+    print("Что нужно сделать?")
+    userValue = input()
+    
+    ToDo[userKey] = userValue
     print ("Событие добавлено")
-      
-  elif userUnswer == "remove":
+  elif userUnswer == "exit":
     print ("Событие удалено")
-  
-  
   elif userUnswer == "show":
-    print ("Ку!")
+    print("\n у вас запланированно:")
+    print(ToDo)
+    for i in ToDo.keys().sorted():
+      print(i + "\t" + ToDo[i])
   else:
     print ("Не корректная команда")
     print ("для вывода списка команд введите help")
