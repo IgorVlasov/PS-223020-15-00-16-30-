c43a8546ec9052c50fa21c5ea57d61896c49fcb1
Artem Biryukov <artem.b_2005@mail.ru>
2021-02-14 13:33:12 +0000

    help me please

--- 01 QnA/todo.py
+++ 01 QnA/todo.py
@@ -12,18 +12,42 @@ print("Enter command")
 print ("for help enter'help'")
 
 while True:
-	userAns = input()
+	userAns = input().lower()
 	if userAns == "exit":
 		print("Programm has stop")
 		break
 	elif userAns == "help":
 		print(help)
 	elif userAns == "add":
-		print("Event has add")
+		print("Enter events date in formate dd.mm.yyyy:")
+		usrKey = input()
+
+		check = usrKey.split('.')
+		for i in check:
+			if i.isdigit():
+				check = True
+			else: 
+				check = False
+				
+
+		if check:
+			print("it is not date!")
+			continue
+
+
+		print("what may you do?")
+		usrValue = input()
+
+		todo [usrKey] = usrValue
+
 	elif userAns == "remove":
 		print("Event has remove")
 	elif userAns == "show":
-		print("abc")
+		print("\nIn your plans:")
+		for i in sorted(todo.keys()):
+			print(i + ':' + todo[i] + "\t")
+			
+		
 	else:
 		print("I don't understand you...")
 		print ("for help enter'help'")
